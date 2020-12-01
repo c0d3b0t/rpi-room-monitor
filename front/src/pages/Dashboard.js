f41e3aa89e95e31dceaa5aecb75d9ea9dc75c3c4
Senik Hakobyan <sen.hakobyan@gmail.com>
2020-12-02 00:45:46 +0400

    refactoring

--- front/src/pages/Dashboard.js
+++ front/src/pages/Dashboard.js
@@ -9,13 +9,13 @@ function Dashboard() {
             </header>
             <div className="App-contents">
                 <div className="App-player">
-                    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width='100%' height='100%'/>
+                    <img src={process.env.REACT_APP_BASE_URL+'/stream'} width='100%' height='100%' alt='Video stream'/>
                 </div>
                 <div className="App-sidebar">
                     <h2>Metrics</h2>
                     <div className="App-metrics">
                         <span className="temperature">Temperature: <br/> 67.1 F / 19.5 C</span>
-                        <span className="humidity">Humidity: 47</span>
+                        <span className="humidity">Humidity: 47%</span>
                         <button className="App-btn"><FaChartBar/> Details</button>
                         <hr/>
                     </div>
