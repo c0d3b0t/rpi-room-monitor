e95b6448c29bf490653f83afad379a4fe74b4606
Senik Hakobyan <sen.hakobyan@gmail.com>
2020-12-13 18:29:32 +0400

    reading from DHT sensor, routing

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+from devices.dht import DhtDevice
+from time import sleep
 
 app = Flask(__name__)
 
@@ -6,6 +9,43 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/stream')
+def stream():
+    pass
+
+
+@app.route('/dht/temperature')
+def dht_temperature():
+    while True:
+        try:
+            dht = DhtDevice()
+            celsius = dht.get_celsius()
+            fahrenheit = dht.get_fahrenheit()
+            break
+        except:
+            sleep(1)
+
+    return jsonify(
+        celsius=celsius,
+        fahrenheit=fahrenheit,
+    )
+
+
+@app.route('/dht/humidity')
+def dht_humidity():
+    while True:
+        try:
+            dht = DhtDevice()
+            humidity = dht.get_humidity()
+            break
+        except:
+            sleep(1)
+
+    return jsonify(
+        humidity=humidity
+    )
 
 
 if __name__ == '__main__':
--- devices/dht.py
+++ devices/dht.py
@@ -6,14 +6,11 @@ class DhtDevice:
     # Init the dht device with pin connected:
     device = adafruit_dht.DHT22(board.D4)
 
-
     def get_celsius(self):
-        return self.device.temperature
-
+        return "{:.1f}".format(self.device.temperature) + ' C'
 
     def get_fahrenheit(self):
-        return self.device.temperature * (9 / 5) + 32
-
+        return "{:.1f}".format(self.device.temperature * (9 / 5) + 32) + ' F'
 
     def get_humidity(self):
-        return self.device.humidity
+        return str(self.device.humidity) + '%'
